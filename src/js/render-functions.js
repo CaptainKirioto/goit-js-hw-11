d267fac2c64dbf32213a7659e1a900c80f9fe7b9
CaptainKirioto <mrg.zhi@gmail.com>
2024-06-01 20:11:50 +0200

    Second try

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,9 +1,29 @@
 // функції для відображення елементів інтерфейсу
 
-function createMarkup() {
-  `
-< li class="gallery-item">
-<img class="gallery-img" alt=${tags}></img>
+export function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+<li class="gallery-item">
+<a class="link" href="${largeImageURL}">
+  <img class="gallery-img" alt=${tags} src=${webformatURL}></img>
+  <div class="stats-container">
+    <p class="img-likes"> <span>Likes:</span> <span>${likes}</span></p>
+    <p class="img-views"> Views: ${views} </p>
+    <p class="img-comments"> Comments: ${comments}</p>
+    <p class="img-downloads"> Downloads: ${downloads}</p>
+</a>
+</div>
 </li >
-`;
+`
+    )
+    .join('');
 }
--- src/main.js
+++ src/main.js
@@ -5,23 +5,33 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-/* ------------------------------- */
+/* --------- Function import --------- */
 
 import { getImage } from './js/pixabay-api';
+import { createMarkup } from './js/render-functions';
+
+/* ------------------------------ */
 
 const form = document.querySelector('.search-form');
-
 form.addEventListener('submit', searchImg);
+const gallery = document.querySelector('.gallery-list');
+const input = form.querySelector('.search-input');
 
 function searchImg(event) {
   event.preventDefault();
-  console.log('boo');
-  const query = event.target.elements.search.value;
+  let query = event.target.elements.search.value;
 
-  getImage(query).then(data => {
-    iziToast.show({
-      title: 'Hey',
-      message: `The ${query} is beautiful`,
+  getImage(query)
+    .then(data => {
+      console.log(data);
+      gallery.innerHTML = '';
+      gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+      event.target.elements.search.value = '';
+    })
+    .catch(error => {
+      console.log(error);
     });
-  });
 }
+//   title: 'Hey',
+//   message: `The ${query} is beautiful`,
+// });
