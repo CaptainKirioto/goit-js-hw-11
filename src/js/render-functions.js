843eaa1530693e42eba762dd6077586934be82ce
CaptainKirioto <mrg.zhi@gmail.com>
2024-06-02 21:32:28 +0200

    something

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,5 +1,18 @@
 // функції для відображення елементів інтерфейсу
 
+/* ----------- SimpleLightbox ----------- */
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+/* ------------------------------ */
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: '250ms',
+  captionsData: 'alt',
+});
+lightbox.refresh();
+
 export function createMarkup(arr) {
   return arr
     .map(
@@ -13,15 +26,15 @@ export function createMarkup(arr) {
         downloads,
       }) => `
 <li class="gallery-item">
-<a class="link" href="${largeImageURL}">
+<a class="gallery-link" href="${largeImageURL}">
   <img class="gallery-img" alt=${tags} src=${webformatURL}></img>
   <div class="stats-container">
     <h5 class="h-likes">Likes:</h5><p class="p-likes">${likes}</p>
     <h5 class="h-likes">Views:</h5><p class="p-views">${views} </p>
     <h5 class="h-likes">Comments:</h5><p class="p-comments">${comments}</p>
     <h5 class="h-likes">Downloads:</h5><p class="p-downloads">${downloads}</p>
-</a>
 </div>
+</a>
 </li >
 `
     )
